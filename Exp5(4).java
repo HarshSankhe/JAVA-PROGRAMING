158bed7fba2721bf378eb9deaae9a266ff040feb
HarshSankhe <harshrsankhe@gmail.com>
2024-10-17 08:28:19 +0530

    Add files via upload

---
+++ Exp5(4).java
@@ -0,0 +1,53 @@
+package skl_b3;
+
+/*
+ * Aim : Write a program in java to count frequency of a given letter in a String.
+ * Name : Harsh Sankhe
+ * UIN : 231P079
+ * Div : A.
+ */
+
+import java.util.*;
+
+
+
+public class CountLetters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char ch='a';
+		int len, count=0;
+		
+		String str = new String();
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Enter a String :");
+		str = sc.nextLine();
+		
+		System.out.println(" String is : "+str);
+		
+		System.out.println("Enter a character to search to compare :");
+		ch = sc.next().charAt(0);
+		
+		len = str.length();
+		
+		for(int i = 0; i<=len-1; i++)
+			
+		if(ch==str.charAt(i))
+		{
+			count++;
+		}
+		System.out.println("Frequency of character"+ch+"in string is "+count);
+		
+		System.out.println("Thank You ");
+		
+		System.out.println("Name : Harsh Sankhe");
+		System.out.println("UIN : 231P079");
+
+	}
+
+}
+
+
